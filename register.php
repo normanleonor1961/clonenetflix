e2926ba28f73607a7b2fc94e7c79e33b281456d0
normanleonor <109060058+normanleonor1961@users.noreply.github.com>
2022-12-07 10:52:18 +0800

    add form and logo

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
+<?php
+    if(isset($_POST["submitButton"])){
+        echo "Form was submitted";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,12 +11,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style/style.css">
-    <title>Register</title>
+    <title>Welcome to Netflix Clone</title>
 </head>
 <body>
     <div class="signInContainer">
         <div class="column">
-            
+
+            <div class="header">
+                <img src="assets/images/leonormanlogo3.png" title='logo' width='200' height='50'alt="">
+                <h3>Sign Up</h3>
+                <span>to continue to Netflix clone</span>
+            </div>
+
+            <form method="POST">
+                    <input type="text" name="firstName" placeholder="First name" required>
+                    <input type="text" name="lastName" placeholder="Last name" required>
+                    <input type="text" name="username" placeholder="Username" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="password" name="password2" placeholder="Confirm password" required>
+                    <input type="submit" name="submitButton" value="SUBMIT">
+            </form>
+           <span>Already have an account?</span> <a href="login.php" class="signInMessage"> &nbsp;  Sign in here!</a>
         </div>
     </div>
     
